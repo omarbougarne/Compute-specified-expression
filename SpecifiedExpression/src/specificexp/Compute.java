dfcede55077932f2071b3f3c2ee8ad4d537b20d6
omarbougarne <omarwimu@gmail.com>
2024-07-11 12:29:48 +0100

    specific computation

---
+++ SpecifiedExpression/src/specificexp/Compute.java
@@ -0,0 +1,10 @@
+package specificexp;
+
+public class Compute{
+	  public static void main(String[] args){
+	    double expression;
+	    expression = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) ;
+	    System.out.print("Expression:\t" + expression);
+	  }
+	}
+
